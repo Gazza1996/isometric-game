2b08b023755bbe186b87af1a255d8ec2ab313480
Gary Mannion <gkmannion@hotmail.com>
2019-01-02 14:51:37 +0000

    refactor

---
+++ Isometric-Assignment/src/ie/gmit/sw/GroundTile.java
@@ -0,0 +1,38 @@
+package ie.gmit.sw;
+
+import java.awt.image.BufferedImage;
+
+import ie.gmit.sw.GroundType;
+
+public class GroundTile {
+	
+	private boolean isWalkable;
+	private GroundType type;
+	
+	public GroundTile() {
+		super();
+	}
+	
+	public GroundTile(Position p, BufferedImage i, boolean w, GroundType type) {
+		super(p, i);
+		this.isWalkable = w;
+		this.type = type;
+	}
+
+	public boolean isWalkable() {
+		return isWalkable;
+	}
+
+	public void setWalkable(boolean isWalkable) {
+		this.isWalkable = isWalkable;
+	}
+
+	public GroundType getType() {
+		return type;
+	}
+
+	public void setType(GroundType type) {
+		this.type = type;
+	}
+	
+}
